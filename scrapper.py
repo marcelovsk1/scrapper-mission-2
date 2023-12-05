47713bdefa39e20f11319228ca57fdd80608df9e
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-05 09:49:12 -0300

    working on

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import facebook
 import time
 import json
 
@@ -11,3 +12,10 @@ driver = webdriver.Chrome(executable_path=driver_path)
 def scrape_website(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+
+
+def scrape_facebook_events(access_token, page_id):
+    graph = facebook.GraphAPI(access_token)
+    events = graph.get_connections(page_id, 'events')
+    # Extract relevant information
+    # ...
