aca5864f06ee773d3eaa93259512e1db0e04f7aa
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-05 09:45:09 -0300

    working on

--- scrapper.py
+++ scrapper.py
@@ -4,3 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import json
+
+driver_path = '/path/to/chromedriver'
+driver = webdriver.Chrome(executable_path=driver_path)
