c95206b619ba259f1e2b4e290f6000f6ee0ca7f6
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-17 14:09:44 -0300

    working on

--- scraper/eventbrite.py
+++ scraper/eventbrite.py
@@ -1,3 +1,26 @@
+# from bs4 import BeautifulSoup
+# import requests
+
+
+# page = requests.get('https://www.eventbrite.com/d/canada--montreal/events/')
+# webpage = BeautifulSoup(page.content, 'html.parser')
+
+# events = webpage.find_all('div', attrs={'class': 'Stack_root__1ksk7'})
+
+# for event in events:
+#     event_name = event.find('h2').text
+#     event_date = event.find('p').text.strip()
+#     event_location = event.find('p', class_='Typography_root__487rx #585163 Typography_body-md__487rx event-card__clamp-line--one Typography_align-match-parent__487rx').text.strip()
+
+#     print(f"{event_name}")
+#     print(f"{event_date}")
+#     print(f"{event_location}")
+#     print("=" * 100)
+
+
+
+# # JSON
+import json
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,32 +29,26 @@ webpage = BeautifulSoup(page.content, 'html.parser')
 
 events = webpage.find_all('div', attrs={'class': 'Stack_root__1ksk7'})
 
+event_list = []
+
 for event in events:
     event_name = event.find('h2').text
     event_date = event.find('p').text.strip()
-    event_location = event.find('p', class_='Typography_root__487rx #585163 Typography_body-md__487rx event-card__clamp-line--one Typography_align-match-parent__487rx').text.strip()
 
-    print(f"{event_name}")
-    print(f"{event_date}")
-    print(f"{event_location}")
-    print("=" * 100)
+    # Verificar se o elemento 'p' com a classe desejada existe
+    event_location_elem = event.find('p', class_='Typography_root__487rx #585163 Typography_body-md__487rx event-card__clamp-line--one Typography_align-match-parent__487rx')
+    event_location = event_location_elem.text.strip() if event_location_elem else None
 
+    event_dict = {
+        'event_name': event_name,
+        'event_date': event_date,
+        'event_location': event_location
+    }
 
-# JSON
-# import json
-# from bs4 import BeautifulSoup
-# import requests
+    event_list.append(event_dict)
 
-# page = requests.get('https://www.eventbrite.com/d/canada--montreal/events/')
-# webpage = BeautifulSoup(page.content, 'html.parser')
+# Converter a lista de dicionários para uma string JSON
+json_data = json.dumps(event_list, indent=2)
 
-# events = webpage.find_all('div', attrs={'class': 'Stack_root__1ksk7'})
-
-# event_names = [event.find('h2').text for event in events]
-
-# data = {'event_names': event_names}
-
-# # Convert to JSON
-# json_data = json.dumps(data, ensure_ascii=False, indent=2)
-
-# print(json_data)
+# Imprimir a string JSON
+print(json_data)
