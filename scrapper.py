88e8e7a6588a9cac4b1bd2ee2d41e2efff0404da
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-17 09:45:30 -0300

    scrapping mission

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -12,3 +12,12 @@ driver = webdriver.Chrome(service=service, options=options)
 
 url = 'https://books.toscrape.com/'
 driver.get(url)
+
+driver.find_element(By.TAG_NAME, 'a')[54].text
+
+result_dict = {'element_texts': element_texts}
+
+result_json = json.dumps(result_dict, indent=2)
+
+print(result_json)
+driver.quit()
