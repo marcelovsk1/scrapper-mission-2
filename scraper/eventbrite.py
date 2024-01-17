3c8536f96c430e615a11ab9f78bd33fa9add0fa4
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-17 11:45:55 -0300

    working on

--- scraper/eventbrite.py
+++ scraper/eventbrite.py
@@ -7,11 +7,12 @@ webpage = BeautifulSoup(page.content, 'html.parser')
 events = webpage.find_all('div', attrs={'class': 'Stack_root__1ksk7'})
 
 event_names = [event.find('h2').text for event in events]
+event_dates = [event.find('p').text.strip() for event in events]
 
-for name in event_names:
-    print(name)
+for name, date in zip(event_names, event_dates):
+    print(f"{name}")
+    print(f"Date: {date}")
     print("=" * 100)
-
 # import json
 # from bs4 import BeautifulSoup
 # import requests
