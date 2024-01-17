ff68a6dd68ef4389d471c779a417bf7d83e260cb
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-17 14:05:27 -0300

    working on

--- scraper/eventbrite.py
+++ scraper/eventbrite.py
@@ -6,13 +6,16 @@ webpage = BeautifulSoup(page.content, 'html.parser')
 
 events = webpage.find_all('div', attrs={'class': 'Stack_root__1ksk7'})
 
-event_names = [event.find('h2').text for event in events]
-event_dates = [event.find('p').text.strip() for event in events]
+for event in events:
+    event_name = event.find('h2').text
+    event_date = event.find('p').text.strip()
+    event_location = event.find('p', class_='Typography_root__487rx #585163 Typography_body-md__487rx event-card__clamp-line--one Typography_align-match-parent__487rx').text.strip()
 
-for name, date in zip(event_names, event_dates):
-    print(f"{name}")
-    print(f"{date}")
+    print(f"{event_name}")
+    print(f"{event_date}")
+    print(f"{event_location}")
     print("=" * 100)
+
 
 # JSON
 # import json
